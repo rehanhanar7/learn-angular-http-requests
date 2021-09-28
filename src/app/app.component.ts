b82f6232318ba8f1cbe085031a92e3bf5ac9dfb5
rehanhanar7 <rehanhanar7@gmail.com>
2021-09-28 20:08:24 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,3 +6,62 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {}
+
+/**
+ * What is Rest API ?
+ * When we hit URL instead of HTML we get data from the url
+ *
+ * Anatomy of Http Requests
+ * URL, Http Verbs, Headers (Meta data), Body (data to be transfered)
+ *
+ * Setup Firebase
+ *
+ * HttpClientModule
+ *
+ * Angular converts Object to JSON
+ *
+ * request works only when we subscribe to the request, Since angular will not call the request since no one is interest in the response
+ *
+ * Browser send two types of requests for post, one is OPTIONS (request type) to check the URL is valid
+ * Second is the actual POST (request type) request
+ *
+ * Get Method to get the data
+ *
+ * Pipe method in subscribe ? - funnel the data through various rxjs operators before it meets the subscribe
+ *
+ * map operator to modify the request and returns observable
+ *
+ * set type of return data in http requests or in argument with POST model (interface)
+ *
+ * Display Data with Loader and Data not found conditions
+ *
+ * Delete Single Data & All Data
+ *
+ * Handling Error at subscribe end of component
+ *
+ * Handling Errpr using Subjects by Storing error as Subjects in Service and we subscribe that subjects in components, will be useful if we need to implement at multiple places
+ *
+ * Handling error using CatchError Operator, will be used to log the error for analytics purpose, catch error is return by throwError method in return statement of CatchError
+ *
+ * Dismiss error in using
+ *
+ * Setting headers in the Http requests
+ *
+ * Adding params in the request (HttpParams) we have to use set here. (we can also use url directly)
+ * We can add multiple params as key value pairs (non object type) using append method in separate [new HttpParams()] variable
+ *
+ * query param differs from server to server, for example in firebase it returns response in more prettier way if we use a query param ('pretty':'print')
+ *
+ * observe property (observe : 'body' | 'response'), we can get more information about the response of the request
+ *
+ * tap operator does some operation on response data but does modify the data
+ * HttpEventType enum in tap events
+ *
+ * Change Response Type, like observe : responseType : 'json' (default) | 'text'
+ *
+ * Interceptors to change request
+ *
+ * Interceptors to change the response
+ *
+ * Multiple Interceptors
+ */
